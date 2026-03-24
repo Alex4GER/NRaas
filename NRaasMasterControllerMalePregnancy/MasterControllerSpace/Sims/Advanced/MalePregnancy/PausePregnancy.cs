8715023557cfd76daa805bda6f468bc73cf827e3
Alex4GER <alexp99@freenet.de>
2026-03-25 00:39:44 +0100

    MasterControllerMalePregnancy 1

---
+++ NRaasMasterControllerMalePregnancy/MasterControllerSpace/Sims/Advanced/MalePregnancy/PausePregnancy.cs
@@ -0,0 +1,65 @@
+﻿using Sims3.Gameplay;
+using Sims3.Gameplay.Abstracts;
+using Sims3.Gameplay.Actors;
+using Sims3.Gameplay.ActorSystems;
+using Sims3.Gameplay.Autonomy;
+using Sims3.Gameplay.CAS;
+using Sims3.Gameplay.Core;
+using Sims3.Gameplay.Interactions;
+using Sims3.Gameplay.Utilities;
+using Sims3.SimIFace;
+using Sims3.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRaas.MasterControllerSpace.Sims.Advanced.MalePregnancy
+{
+    public class PausePregnancy : SimFromList, IPregnancyOption
+    {
+        public override string GetTitlePrefix()
+        {
+            return "PauseMalePregnancy";
+        }
+
+        protected override int GetMaxSelection()
+        {
+            return 0;
+        }
+
+        protected override bool CanApplyAll()
+        {
+            return true;
+        }
+
+        protected override bool PrivateAllow(SimDescription me)
+        {
+            if (!base.PrivateAllow(me)) return false;
+
+            if (me.CreatedSim == null) return false;
+
+            if (me.IsPregnant) return false;
+
+            if (me.IsHuman && me.IsMale && me.IsVisuallyPregnant)
+            {
+                if (me.CreatedSim.BuffManager.HasElement(BuffNames.MalePregnancy))
+                {
+                    BuffInstance buffInstance = me.CreatedSim.BuffManager.GetElement(BuffNames.MalePregnancy);
+                    return !buffInstance.mTimeoutPaused;
+                }
+            }
+
+            return false;
+        }
+
+        protected override bool Run(SimDescription me, bool singleSelection)
+        {
+            if (me.IsVisuallyPregnant)
+            {
+                me.CreatedSim.BuffManager.PauseBuff(BuffNames.MalePregnancy);
+            }
+
+            return true;
+        }
+    }
+}
