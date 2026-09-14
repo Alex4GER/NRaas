08c8f18bc881fb4fb38b29743c85b4a0afc7bf89
Alex4GER <alexp99@freenet.de>
2026-09-14 15:41:33 +0200

    add PregnancyController project to suite

---
+++ NRaasPregnancyController/PregnancyController.cs
@@ -0,0 +1,15 @@
+﻿using Sims3.SimIFace;
+
+namespace NRaas
+{
+    public class PregnancyController : Common
+    {
+        [Tunable, TunableComment("Scripting Mod Instantiator, value does not matter, only its existence")]
+        protected static bool kInstantiator = false;
+
+        static PregnancyController()
+        {
+            Bootstrap();
+        }
+    }
+}
---
+++ NRaasPregnancyController/VersionStamp.cs
@@ -0,0 +1,15 @@
+﻿using Sims3.Gameplay.Abstracts;
+using Sims3.Gameplay.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRaas
+{
+    public class VersionStamp : Common.ProtoVersionStamp
+    {
+        public static readonly string sNamespace = "NRaas.PregnancyController";
+
+        public static readonly int sVersion = 1;
+    }
+}
